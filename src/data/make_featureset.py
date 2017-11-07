06af615d27f7cb8eef7eae01412a07e2c3f55a50
napoleon <stephen.pettinato@gmail.com>
2017-11-06 22:08:51 -0800

    Updates to produce feature set, still having some trouble with running make_featureset because of some import issue

--- src/data/make_featureset.py
+++ src/data/make_featureset.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+# Run script as python3 -m make_featureset
+# since it's inside of this module.
+
 import os
-from src.features import build_features
+
+# Having trouble with this '-m' didn't help
+from ..features import build_features as bf
 
 
 def main(input_filepath, output_filepath):
@@ -9,16 +14,16 @@ def main(input_filepath, output_filepath):
     :param input_filepath: Raw input Titanic training set.
     :param output_filepath: Output filename for constructed features
     """
-    raw_df = build_features.read_raw_data(input_filepath)
-    features_df = build_features.build_features(raw_df)
+    raw_df = bf.read_raw_data(input_filepath)
+    features_df = bf.build_features(raw_df)
     features_df.to_csv(output_filepath, index=False)
 
 
 if __name__ == '__main__':
-    print(__file__)
-
-    os.basename()
-
-    input_filepath = '../../data/raw/train.csv'
-    output_filepath = '../../data/processed/feature_set.csv'
-    #main(input_filepath, output_filepath)
+    proj_dir = os.path.abspath(os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), '../../'))
+    input = os.path.join(proj_dir, 'data/raw/train.csv')
+    output = os.path.join(proj_dir, 'data/raw/feature_set.csv')
+    print('Reading input data from {}'.format(input))
+    print('Writing Features to file {}'.format(output))
+    main(input, output)
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,7 +1,5 @@
 
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 
 
 def read_raw_data(filename):
@@ -28,48 +26,21 @@ def build_features(raw_df):
         1. Survived (dependent variable)
         2. Pclass - dummify
         3. Sex - 1 if male, 0 if female
-        4. Fare - Normalized - Will need to store the mean/stdev
-           to a file in order to normalize the test set.
+        4. Fare - not transformed
         5. Cabin - as 5 columns.  1 if nan, 1 each for if contains B, C, D, E
         6. Embarked - dummify without nan dummy column
     :param df: Data Frame with the above columns
-    :return: New DataFrame with features as above.
+    :return: New DataFrame with features as above and all other columns dropped
     All columns returned will be numeric data types.
     """
-    # TODO
+    req_cols = ['Survived', 'Pclass', 'Sex', 'Fare', 'Cabin', 'Embarked']
+    features_df = pd.DataFrame.copy(raw_df[req_cols])
+    features_df = pd.get_dummies(features_df, columns=['Pclass', 'Embarked'])
+    features_df['Sex'] = (features_df['Sex'] == 'male').astype(int)
+    features_df['Cabin_nan'] = features_df.Cabin.isnull().astype(int)
 
-    features_df = pd.DataFrame.copy(raw_df)
-
-    #features_df.Pclass = normalize(features_df.Pclass)
-    #features_df.SibSp = normalize(features_df.SibSp)
-    #features_df.Fare = normalize(features_df.Fare)
-
-    # # Transform Cabin
-    # for cabin_letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'T']:
-    #     features_df['Cabin_' + cabin_letter] = features_df.Cabin.str.contains(cabin_letter).fillna(False).astype(int)
-    #
-    # features_df['Cabin_Number'] = features_df.Cabin.str.extract('[A-Z]([0-9]*)', expand=False)
-    # features_df.Cabin_Number = features_df.Cabin_Number.replace('', np.nan)
-    #
-    # # Fill the NaN Cabin Numbers with the max Cabin Number.  Assumes that lower cabin numbers are better.
-    # features_df['Cabin_Number_NaN'] = features_df.Cabin_Number.isnull().astype(int)
-    # features_df.loc[features_df.Cabin_Number.isnull(), ('Cabin_Number')] = features_df.Cabin_Number.max()
-    #
-    # # Fill the missing Cabin_Numbers with the Cabin_Number mean
-    # features_df.Cabin_Number = features_df.Cabin_Number.fillna(features_df.Cabin_Number.mean())
-    # features_df.Cabin_Number = features_df.Cabin_Number.astype(int)
-    #
-    # # Transform Sex
-    # features_df['Sex'] = (features_df['Sex'] == 'male').astype(int)
-    #
-    # # Transform Embarked
-    # features_df = pd.get_dummies(features_df, columns=['Embarked'], dummy_na=True)
-    #
-    # # Age is sometimes null, so replace the null values with the mean
-    # features_df['Age'] = features_df.Age.fillna(features_df.Age.mean())
-    #
-    # features_df = features_df.drop(labels=['Cabin'], axis=1)
-    #
-    # assert 0 == features_df.isnull().sum().sum(), 'All cells should be non-nan'
-
+    for cabin in ['B', 'C', 'D', 'E']:
+        features_df['Cabin_' + cabin] = \
+            features_df.Cabin.fillna('').str.contains(cabin).astype(int)
+    features_df = features_df.drop(labels=['Cabin'], axis=1)
     return features_df
